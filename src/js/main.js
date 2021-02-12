0be15412b5dd00e09bdcdc4cb3d46cdbd5c16f58
Jon Stump <jmstump@gmail.com>
2021-02-12 08:50:50 -0800

    inital code for pulling from API

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,18 @@
-import '../css/styles.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
 
+function getElements(response) {
+  if (response) {
+    console.log(response);
+    $('#currencyConverted').text(`Conversion: ${} `);
+  } else {
+    $('#error').text('error message here');
+  }
+  
+}
+
+$(document).ready(function() {
+
+})
