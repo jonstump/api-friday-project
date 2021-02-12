8ea35a168c826569fefa1ce8dfc6ce311d6eeb6a
Jon Stump <jmstump@gmail.com>
2021-02-12 10:25:49 -0800

    testing code for user input

--- src/js/main.js
+++ src/js/main.js
@@ -5,27 +5,26 @@ import '../css/styles.css';
 import Currency from "../js/currency.js";
 
 
-
-function getElements(response) {
-  if (response && response!="Error") {
-    console.log(response);
-    // $('#currencyConverted').text(`Conversion: ${response.conversion_rates.lookUp} `);
-  } else {
-    $('#error').text('there was an error');
-  }
-  
-}
-
 $(document).ready(function() {
   $('form').submit(function() {
     let selectedCurrency = $('#selectedCurrency').val();
     let lookUp = Currency.currencyLookUp(selectedCurrency);
+    console.log(lookUp);
   });
   Currency.currencyConvert()
-    .then(function(response, error) {
-      getElements(response, error);
+    .then(function(response) {
+      getElements(response);
     });
-  
+
+  function getElements(response) {
+    if (response && response!="Error") {
+      console.log(response);
+      $('#currencyConverted').text(`Conversion: ${lookUp} `);
+    } else {
+      $('#error').text('there was an error');
+    }
+    
+  }
   
 
 });
