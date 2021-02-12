79ef4f1aa819ca52cfd1fbf8c857e10849ed6fbc
Jon Stump <jmstump@gmail.com>
2021-02-12 14:03:19 -0800

    working code to convert rates for user entry

--- src/js/main.js
+++ src/js/main.js
@@ -4,25 +4,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/styles.css';
 import Currency from "../js/currency.js";
 
-
-
 $(document).ready(function() {
   $('#convert').click(function() {
+    let amount = $('#dollarAmount').val();
     let currency = $('#selectedCurrency').val();
     
-    let promise = Currency.currencyConvert(currency);
+    let promise = Currency.currencyConvert();
     console.log(promise);
 
     promise.then(function(response) {
       const body = JSON.parse(response);
-      console.log(body)
-      console.log(body.result)
-      console.log(body.conversion_rates)
-      console.log(body.conversion_rates['AED'])
+      const rate = body.conversion_rates[currency]
+      console.log(amount * body.conversion_rates[currency])
       if (body.result === "error") {
         $('#error').text(`There was an error processing your request: ${body['error-type']}`);
+      // } else if (currency != body.conversion_rates[currency]) {
+      //   $('#conversion').text(`Sorry we can't cover a rate for ${currency}`)
       } else {
-        $('#conversion').text(`The current rate for ${currency} is ${body}%`)
+        let convertedRate = rate * amount
+        $('#conversion').text(`The current rate for ${currency} is $${convertedRate}`)
       }
     }, function(error) {
       
